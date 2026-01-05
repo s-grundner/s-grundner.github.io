d20700517f3a697f09650e57acb3735b3d557e12
s-grundner <simon.grundner@gmail.com>
2026-01-06 00:00:46 +0100

    Update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://s-grundner.github.io/OpenStemBook-Astro/',
+	site: 'https://s-grundner.github.io',
 	vite: {
 		plugins: [tailwindcss()],
 	},
